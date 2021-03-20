ec282a5951d3603e38c59c20c1a0a073fddf04cf
le-grand-mannitout <ouaksel.madil@gmail.com>
2021-03-20 12:35:55 +0100

    simplify code by removing unecessary functions arguments

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ KEY_A = 97
 KEY_ENTER = 10
 KEY_SPACE = 32
 
-def main_menu(stdscr,
-              reaction_time: float,
-              variation: float,
-              nb_tries_bf_change: int):
+def main_menu(stdscr, reaction_time: float):
     """
         Main curses menu that gets user input
     """
@@ -29,48 +26,38 @@ def main_menu(stdscr,
         if k in (KEY_A, KEY_ENTER, KEY_SPACE):
             time.sleep(reaction_time)
 
-            nb_tries = flash(stdscr, nb_tries, Config.flash_time)
-            reaction_time = limit_tries(nb_tries_bf_change,
-                                        reaction_time,
-                                        nb_tries, variation)
+            nb_tries = flash(stdscr, nb_tries)
+            reaction_time = limit_tries(nb_tries, reaction_time)
 
         stdscr.refresh()
         k = stdscr.getch()
 
 
-def flash(stdscr,
-          nb_tries: int,
-          flash_time: float) -> int:
+def flash(stdscr, nb_tries: int) -> int:
     """
         White flash appears on screen
     """
     stdscr.bkgd(" ", curses.color_pair(2))
     stdscr.refresh()
-    time.sleep(flash_time)
+    time.sleep(Config.flash_time)
     stdscr.bkgd(" ", curses.color_pair(1))
 
     return nb_tries + 1
 
 
-def limit_tries(nb_tries_bf_change: int,
-                reaction_time: float,
-                nb_tries: int,
-                variation: float) -> float:
+def limit_tries(nb_tries: int, reaction_time: float) -> float:
     """
         Determine if limit of first time sequence is reach and
         adapt reaction time if this is the case
     """
-    if nb_tries == nb_tries_bf_change:
-        reaction_time -= variation
+    if nb_tries == Config.nb_tries_bf_change:
+        reaction_time -= Config.variation
 
     return reaction_time
 
 
 def main():
-    curses.wrapper(main_menu,
-                   Config.reaction_time,
-                   Config.variation,
-                   Config.nb_tries_bf_change)
+    curses.wrapper(main_menu, Config.reaction_time)
 
 
 if __name__ == "__main__":
